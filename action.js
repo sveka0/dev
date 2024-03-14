810706ba7247bc21f21eeab82f2ee545f500ef2c
Cold <sveka0@gmail.com>
2024-03-14 23:30:57 +0200

    Добавьте файлы проекта.

---
+++ action.js
@@ -0,0 +1,33 @@
+let str = "Awesome text";
+let str2 = "Awesome";
+console.log(str);
+
+console.log(str.indexOf(str2));
+
+var text = str + " cu cu " + str2;
+console.log(text);
+
+let name = prompt("Please enter your name");
+console.log(name);
+
+let age = prompt("Please enter your age");
+
+console.log(age);
+
+let age2 = parseInt(age);
+
+console.log(age2);
+
+let password = prompt("Please enter your password");
+
+console.log(password);
+
+
+
+
+
+
+
+
+
+
